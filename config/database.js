9ad21502184c97e27a0222d4a6441dc6bb704ff7
Chandrek Bisht <chandrek07@gmail.com>
2025-08-14 12:13:18 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,17 +1,19 @@
-const path = require('path');
-
-module.exports = ({env}) => ({
+// path: ./config/database.js
+module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      connectionString: env('DATABASE_URL'),
-      host: env('DATABASE_HOST', 'dpg-cj8gnsljeehc73bcqjh0-a.singapore-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'simpleone_1z1r'),
-      user: env('DATABASE_USERNAME', 'simpleone_1z1r_user'),
-      password: env('DATABASE_PASSWORD', 'nv5PWOLiXjgJPyQ0e7mvvCtS1hpXdEGn'),
-      ssl: env.bool('DATABASE_SSL', true),
-
+      host: env(
+        "DATABASE_HOST",
+        "dpg-d2bmscvdiees73etu700-a.oregon-postgres.render.com"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "renderdb_cmii"),
+      user: env("DATABASE_USERNAME", "renderdb_cmii_user"),
+      password: env("DATABASE_PASSWORD", "fMZyH8CYLPKITgIFDe1kxufQUFajl3dz"),
+      ssl: env.bool("DATABASE_SSL", true)
+        ? { rejectUnauthorized: false }
+        : false,
     },
   },
 });
