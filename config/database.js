8bcb8fc11f9f56d58db5f7f0c24edeb739adbccd
Chandrek Bisht <chandrek07@gmail.com>
2026-05-11 12:30:33 +0530

    railway setup

--- config/database.js
+++ config/database.js
@@ -1,19 +1,11 @@
-// path: ./config/database.js
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "dpg-d2bmscvdiees73etu700-a.oregon-postgres.render.com"
-      ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "renderdb_cmii"),
-      user: env("DATABASE_USERNAME", "renderdb_cmii_user"),
-      password: env("DATABASE_PASSWORD", "fMZyH8CYLPKITgIFDe1kxufQUFajl3dz"),
-      ssl: env.bool("DATABASE_SSL", true)
-        ? { rejectUnauthorized: false }
-        : false,
+      connectionString: env("DATABASE_URL"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
